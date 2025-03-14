1683dfc5078815e777c0e80de50259116bf1b63f
Rebecca Benton <becky@beckybenton.com>
2025-03-14 15:00:59 -0700

    Improved LongTaskABC.py

--- LongTaskABC.py
+++ LongTaskABC.py
@@ -1,39 +1,96 @@
+import uuid
 from abc import ABC, abstractmethod
 
 
 class LongTaskABC(ABC):
     """
-    The Command interface declares a method for executing a command.
-    This is designed for long-running tasks.
+    This is our standard interface for any long-running job—whether it's copying files,
+    processing images, or anything else—and your UI can interact with these jobs in
+    a uniform way. It is, essentially, the Command Pattern.
+
+    It defines the following methods:
+
+    - execute(): Runs the job.
+    - get_progress(): Returns an integer (0–100) representing job progress.
+    - cancel(): Stops the job.
+    - is_working(): Indicates if the job is currently running.
+    - failed() and get_failure(): Allow error reporting.
+
+    A task might be run asynchronously:
+    ```python
+    def start_task(self, task: LongTaskABC) -> str:
+        self.tasks[task.get_job_id()] = task  # add to dictionary of tasks
+        threading.Thread(target=task.execute).start()
+        return task.get_job_id()
+    ```
     """
+
+    def __init__(self, job_id=None):
+        """
+
+        :param job_id: a unique identifier for the job; if None,
+        a uuid will be generated and assigned.
+        """
+        self._working: bool = False
+        self._success: bool = False  # completed 100%
+        self._progress: int = 0
+        self._cancelled: bool = False
+        self._failed: bool = False
+        self._failure: Exception = None
+        self._job_id: str = job_id
+
+        if self._job_id is None:
+            self._job_id = str(uuid.uuid4())
 
     @abstractmethod
     def execute(self) -> None:
+        """
+        Example implementation:
+        ```python
+        self._working = True
+        try:
+            # Simulate progress in 10 steps over 5 seconds.
+            for i in range(1, 11):
+                if self._cancelled:
+                    break
+                time.sleep(0.5)
+                self._progress = i * 10
+            self._working = False
+        except Exception as e:
+            self._failed = True
+            self._failure = e
+            self._working = False
+        ```
+
+        :return: None
+        """
         pass
 
-    @abstractmethod
     def get_progress(self) -> int:
         """
         :return: The current progress of the command, from 0..100, inclusive.
         """
-        pass
+        return self._progress
 
-    @abstractmethod
-    def cancel(self) -> None:
-        """
-        Cease working, if you are working.
-        :return: Nothing.
-        """
-        pass
-
-    @abstractmethod
     def is_working(self) -> bool:
-        pass
+        return self._working
 
-    @abstractmethod
+    def was_victorious(self) -> bool:
+        return self._success
+
+    def cancel(self) -> None:
+        self._working = False
+        self._cancelled = True
+
+    def is_canceled(self) -> bool:
+        return self._cancelled
+
     def failed(self) -> bool:
-        pass
+        self._working = False
+        return self._failed
 
-    @abstractmethod
     def get_failure(self) -> Exception:
-        pass
+        return self._failure
+
+    def get_job_id(self) -> str:
+        return self._job_id
