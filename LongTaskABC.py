bb6ba4c8c364a5f15f06f0401bd598a6f79674f1
Rebecca Benton <becky@beckybenton.com>
2025-03-13 21:15:24 -0700

    First draft of LongTaskABC.py

---
+++ LongTaskABC.py
@@ -0,0 +1,31 @@
+from abc import ABC, abstractmethod
+
+
+class LongTaskABC(ABC):
+    """
+    The Command interface declares a method for executing a command.
+    This is designed for long-running tasks.
+    """
+
+    @abstractmethod
+    def execute(self) -> None:
+        pass
+
+    @abstractmethod
+    def get_progress(self) -> int:
+        """
+        :return: The current progress of the command, from 0..100, inclusive.
+        """
+        pass
+
+    @abstractmethod
+    def cancel(self) -> None:
+        """
+        Cease working, if you are working.
+        :return: Nothing.
+        """
+        pass
+
+    @abstractmethod
+    def is_working(self) -> bool:
+        pass
